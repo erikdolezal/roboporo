624d1670da1e81d81c8cde6b55d362c8083aad4d
dodo <erik.dolezal@seznam.cz>
2025-11-05 13:21:48 +0100

    option for local

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import argparse
 from ctu_crs import CRS97, CRS93
 from src.interface.robot_interface import RobotInterface
 
-
 def main(args):
+    tty_dev = None if args.local else "/dev/mars"
     if args.robot == "CRS97":
-        robot = RobotInterface(CRS97())
+        robot = RobotInterface(CRS97(tty_dev=tty_dev))
     elif args.robot == "CRS93":
-        robot = RobotInterface(CRS93())
-    robot.initialize(home = args.home)
-
-    
-
-
+        robot = RobotInterface(CRS93(tty_dev=tty_dev))
+    if not args.local:
+        robot.initialize(home = args.home)
+        robot.calibrate_camera()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Initialize CRS Robot")
@@ -28,5 +26,12 @@ if __name__ == "__main__":
         action="store_true",
         help="Home the robot after initialization",
     )
+
+    parser.add_argument(
+        "--local",
+        action="store_true",
+        help="Use class in local",
+    )
+
     args = parser.parse_args()
     main(args)
